1b83152c629b0b25706a6ae54a44ba80ced5032c
Shreya Malogi <5hre9a@gmail.com>
2021-03-22 19:47:04 +0530

    HR

---
+++ F_for loop.java
@@ -0,0 +1,25 @@
+//https://www.hackerrank.com/challenges/java-loops-i/problem
+//author: shreyamalogi
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+        int i;
+        for(i=1;i<=10;i++)
+        {
+            System.out.println(+N +" x " +i +" = "  +N*i);
+        }
+        scanner.close();
+    }
+}
