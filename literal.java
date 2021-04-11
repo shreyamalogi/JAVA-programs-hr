644c5cc57e5f5c0d173a283ef0a7237c0caef38f
Shreya Malogi <5hre9a@gmail.com>
2021-04-11 20:12:11 +0530

    CWH

---
+++ literal.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class literal {
+    public static void main(String[] args) {
+        byte age = 19;
+        int age2 = 32;
+        short age3 = 57;
+        long ageDino = 372343739l;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66d;
+        boolean a = true;
+        System.out.println(age);
+        String str = "tina";
+        System.out.println(str);
+    }
+}
